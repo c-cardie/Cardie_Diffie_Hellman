ee878ced6c0189f125efe32af2b8e5c33866c0c6
c-cardie <ccardie001@student.franciscan.edu>
2024-12-06 10:08:27 -0500

    finds Alice's and Bob's private keys

--- Cardie_Diffie_Hellman.py
+++ Cardie_Diffie_Hellman.py
@@ -1,13 +1,24 @@
-
 import Crypto
 from Crypto.Util import number
+from sympy import primitive_root
+import random
 
+#returns an n-bit prime number
 def n_bit_prime(n):
   return number.getPrime(n)
+#p is the the n-bit prime number
+p = n_bit_prime(128)
 
-from sympy import primitive_root
-g = primitive_root(n_bit_prime(256))
-print(g)
+#find g, primitive root of p
+g = primitive_root(p)
+
+#Alice's private key
+a = random.randint(1, p)
+print("a = ", a)
+
+#Bob's private key
+b = random.randint(1, p)
+print("b = " , b)
 
 
 
